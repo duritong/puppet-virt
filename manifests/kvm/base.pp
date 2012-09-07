febc8015ad6e598f7ddf14d418e99ebec218be5d
mh <mh@immerda.ch>
2012-09-07 19:29:02 +0200

    on c5 it's called that way

--- manifests/kvm/base.pp
+++ manifests/kvm/base.pp
@@ -2,4 +2,10 @@ class virt::kvm::base {
   package{ [ 'kvm', 'qemu-kvm']:
     ensure => present,
   }
+
+  if $operatingsystem == 'CentOS' && $lsbmajdistrelease == '5' {
+    Package['qemu-kvm']{
+      name => 'kvm-qemu-img'
+    } 
+  }
 }
