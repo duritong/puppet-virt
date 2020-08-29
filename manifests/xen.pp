5f590931a937e1c05a1a36b6b868c4587340e5bf
o <o@immerda.ch>
2020-08-29 19:17:32 +0200

    fix xen for qubes vms

--- manifests/xen.pp
+++ manifests/xen.pp
@@ -2,6 +2,9 @@ class virt::xen {
   case $::virtual {
     'xen0': { include virt::xen::xen0 }
     'xenu': { include virt::xen::xenu }
+    'xenhvm': {
+      # this currently happens in qubes vms
+    }
     default: { fail("No such xen mode known") }
   }
 }
