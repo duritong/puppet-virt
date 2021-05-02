20061aca5956651f6b6ed458a82801dd791be141
mh <mh@immerda.ch>
2021-05-02 14:08:55 +0200

    modernize to only manage what is still needed

--- manifests/libvirt/base.pp
+++ manifests/libvirt/base.pp
@@ -1,37 +1,18 @@
 # basic stuff for a libvirt host
 class virt::libvirt::base {
   require rubygems::xmlsimple
-  package { ['libvirt', 'python-virtinst', 'ruby-libvirt', 'virt-top']:
+  package { ['libvirt', 'virt-install', 'virt-top']:
     ensure => installed,
-  }
-
-  if ($facts['os']['name'] == 'CentOS') and versioncmp($facts['os']['release']['major'],'6') > 0 {
-    Package['ruby-libvirt'] {
-      name => 'rubygem-ruby-libvirt'
-    }
-    Package['python-virtinst'] {
-      name => 'virt-install'
-    }
-  }
-
-  service { 'libvirtd':
+  } -> service { 'libvirtd':
     ensure    => running,
     enable    => true,
     hasstatus => true,
-    require   => Package['libvirt'],
-  }
-
-  file {
+  } -> file {
     '/usr/local/sbin/guest_starter':
       source  => 'puppet:///modules/virt/libvirt/guest_starter',
       require => Package['libvirt'],
       owner   => root,
       group   => 0,
       mode    => '0700';
-    '/usr/local/sbin/generate_random_macaddress':
-      source => 'puppet:///modules/virt/libvirt/random_mac_address.rb',
-      owner  => root,
-      group  => 0,
-      mode   => '0755';
   }
 }
