1aae630110829031b854dc9ac381ad5490c94e74
o <o@immerda.ch>
2021-10-02 18:18:23 +0200

    this fact changed with qubes switching to pvm

--- manifests/xen.pp
+++ manifests/xen.pp
@@ -2,7 +2,7 @@ class virt::xen {
   case $::virtual {
     'xen0': { include virt::xen::xen0 }
     'xenu': { include virt::xen::xenu }
-    'xenhvm': {
+    'xen': {
       # this currently happens in qubes vms
     }
     default: { fail("No such xen mode known") }
